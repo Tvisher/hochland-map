e280e15e5f395e99f394192bb1e5e94a5838b13d
Ivan Tvisher <Tvisher@mail.ru>
2024-06-29 16:23:52 +0500

    add so,e fix

--- src/components/FirstStep.vue
+++ src/components/FirstStep.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="step-wrapper">
+  <div class="step-wrapper" :class="[isInteractive ? '' : 'no-interactive']">
     <div class="step-wrapper__content">
-      <div class="interactive-item" data-item="1">
-        <img src="@/assets/img/modules/module-1/object-6.svg" alt="" />
-      </div>
+      <transition name="fade">
+        <div
+          class="interactive-item"
+          :class="[moduleStep == 1 ? 'pulse' : '']"
+          data-item="1"
+          @click="showMessage"
+          v-if="moduleStep < 2"
+        >
+          <img src="@/assets/img/modules/module-1/object-6.svg" alt="" />
+        </div>
+      </transition>
 
       <div class="interactive-item" data-item="2">
         <img src="@/assets/img/modules/module-1/object-1.svg" alt="" />
@@ -13,31 +21,243 @@
         <img src="@/assets/img/modules/module-1/object-2.svg" alt="" />
       </div>
 
-      <div class="interactive-item" data-item="4">
+      <div
+        class="interactive-item"
+        data-item="4"
+        :class="[moduleStep == 2 ? 'pulse' : '']"
+        @click="showSecondStepModal"
+      >
         <img src="@/assets/img/modules/module-1/object-3.svg" alt="" />
       </div>
 
-      <div class="interactive-item" data-item="5">
+      <div
+        class="interactive-item"
+        data-item="5"
+        :class="[moduleStep == 3 ? 'pulse' : '']"
+        @click="showVideoModal"
+      >
         <img src="@/assets/img/modules/module-1/object-4.svg" alt="" />
       </div>
 
       <div class="interactive-item" data-item="6">
         <img src="@/assets/img/modules/module-1/object-5.svg" alt="" />
       </div>
+
+      <transition name="fade-scale" mode="out-in">
+        <div class="man-modal" v-if="showMessageModal">
+          <div class="man-modal__close" @click="closeModalAndNextStep"></div>
+          Дорогой друг, я так ждал тебя. Сегодня мы отправляемся в путешествие к
+          сыроварне «Хохланда». И я хочу рассказать тебе об этой компании... до
+          встречи!
+        </div>
+      </transition>
+
+      <div
+        class="arrow-template"
+        :class="[moduleStep == 1 ? 'show' : '']"
+        data-item="1"
+        v-if="moduleStep < 2"
+      ></div>
+      <div
+        class="arrow-template"
+        :class="[moduleStep == 2 ? 'show' : '']"
+        data-item="2"
+        v-if="moduleStep == 2"
+      ></div>
+
+      <div
+        class="arrow-template"
+        :class="[moduleStep == 3 ? 'show' : '']"
+        data-item="3"
+        v-if="moduleStep == 3"
+      ></div>
+
+      <transition name="fade" mode="out-in">
+        <div class="modal-template tabs-modal" v-if="isShowSecondStepModal">
+          <div class="modal-content">
+            <div
+              class="man-modal__close"
+              @click="isShowSecondStepModal = false"
+            ></div>
+            <div class="tabs-modal__inner">
+              <div class="tabs-head">
+                <div
+                  class="tabs-btn"
+                  :class="[activeTab == index ? 'current' : '']"
+                  v-for="(tabItem, index) in tabsList"
+                  @click="showCurrentTab(index)"
+                >
+                  {{ tabItem.tabName }}
+                </div>
+              </div>
+              <div class="tabs-content">
+                {{ tabsList[activeTab].tabContent }}
+              </div>
+              <div
+                class="modal-btn"
+                :class="[!isShowAlTabs ? 'pointer-none' : '']"
+                @click="setNextStep(3)"
+              >
+                Прочитано
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+
+      <transition name="fade" mode="out-in">
+        <div class="modal-template video-modal" v-if="isShowVideoModal">
+          <div class="modal-content">
+            <div
+              class="man-modal__close"
+              @click="isShowVideoModal = false"
+            ></div>
+            <div class="video-block">
+              <vue-plyr>
+                <video crossorigin playsinline poster="@/assets/img/poster.png">
+                  <source src="@/assets/files/video-ex.mp4" type="video/mp4" />
+                </video>
+              </vue-plyr>
+            </div>
+          </div>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted, computed } from "vue";
+const moduleStep = ref(0);
+const isInteractive = ref(false);
+const showMessageModal = ref(false);
+const isShowSecondStepModal = ref(false);
+const isShowVideoModal = ref(false);
+
+const activeTab = ref(0);
+const featuredTabs = ref([0]);
+
+const isShowAlTabs = computed(() => {
+  return featuredTabs.value.length == tabsList.length;
+});
+
+const tabsList = [
+  {
+    tabName: "Лист 1",
+    tabContent:
+      "Ты уже определился с корпоративной связью и подал заявку в ServiceDesk, предварительно согласовав с непосредственным руководителем и прикрепив письмо о согласовании к заявке? Если нет, то пора сделать.",
+  },
+  {
+    tabName: "Лист 2",
+    tabContent:
+      "редварительно согласовав с непосредственным руководителем и прикрепив письмо о согласовании к заявке? Если нет, то пора сделать.",
+  },
+  {
+    tabName: "Лист 1",
+    tabContent:
+      "Ты уже определился с корпоративной  предварительно согласовав с непным руководителем и прикрепив письмо о согласовании к заявке? Если нет, то пора сделать.",
+  },
+];
+
+const showMessage = () => {
+  if (moduleStep.value == 1) {
+    showMessageModal.value = true;
+    moduleStep.value = 0;
+  }
+};
+
+const showVideoModal = () => {
+  isShowVideoModal.value = moduleStep.value < 3 ? false : true;
+};
+
+const closeModalAndNextStep = () => {
+  showMessageModal.value = false;
+  moduleStep.value = 2;
+};
+
+const showSecondStepModal = () => {
+  isShowSecondStepModal.value = true;
+};
+
+const setNextStep = (step) => {
+  if (step == 3) {
+    isShowSecondStepModal.value = false;
+    moduleStep.value = 3;
+  }
+};
+
+const showCurrentTab = (ind) => {
+  activeTab.value = ind;
+  if (!featuredTabs.value.includes(ind)) {
+    featuredTabs.value.push(ind);
+  }
+};
+
+onMounted(() => {
+  setTimeout(() => {
+    moduleStep.value = 1;
+    isInteractive.value = true;
+  }, 800);
+});
+</script>
 
 <style lang="scss" scoped>
 .step-wrapper__content {
   background-image: url(@/assets/img/modules/module-1/module-1-bg.svg);
 }
 
+.arrow-template {
+  position: absolute;
+  background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg width='123' height='215' viewBox='0 0 123 215' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cmask id='mask0_2109_5305' style='mask-type:alpha' maskUnits='userSpaceOnUse' x='0' y='0' width='123' height='215'%3e%3crect width='123' height='215' fill='%23D9D9D9'/%3e%3c/mask%3e%3cg mask='url(%23mask0_2109_5305)'%3e%3cg filter='url(%23filter0_d_2109_5305)'%3e%3cpath d='M87.9582 141.996L122.182 138.826L55.3048 213.776L16.3366 130.375L53.1999 139.875C53.1999 139.875 85.3189 64.9363 11.6033 1.59618C11.6033 1.59618 99.0172 34.3266 87.9582 141.996Z' fill='white'/%3e%3cpath d='M87.8383 142.117C98.0684 141.373 112.148 140.466 122.259 139.657L121.561 138.279C99.5735 162.253 75.7978 188.556 54.2251 212.814L56.6136 213.164C44.235 187.113 29.108 156.252 16.8057 130.168L16.2058 130.889C26.8613 133.666 42.2501 137.591 53.0473 140.477C56.69 135.535 57.3989 127.632 59.0239 121.477C68.5451 76.0544 47.7577 29.2216 12.7377 0.273624L10.9927 3.2362C65.8185 26.74 93.7931 83.9364 87.8383 142.117ZM88.0673 141.887C96.5742 87.8719 71.2062 30.3039 22.0843 4.69016C19.7286 3.55323 7.49174 -4.06639 10.4582 2.91917C50.0808 34.8516 70.7482 90.7033 52.6437 139.646L53.3417 139.296C42.37 137.055 26.3378 131.469 15.835 130.244C15.5951 130.649 25.4653 151.135 25.4762 151.507C33.9176 170.19 45.4347 195.76 53.9852 214.388C55.7847 218.772 70.181 197.957 73.0821 195.99C88.6017 178.487 107.491 157.072 122.804 139.384C125.967 135.721 90.8921 142.335 88.0782 141.887L88.0673 141.887Z' fill='black'/%3e%3c/g%3e%3c/g%3e%3cdefs%3e%3cfilter id='filter0_d_2109_5305' x='-0.00244141' y='0' width='123.008' height='215' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'%3e%3cfeFlood flood-opacity='0' result='BackgroundImageFix'/%3e%3cfeColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' result='hardAlpha'/%3e%3cfeOffset dx='-10'/%3e%3cfeComposite in2='hardAlpha' operator='out'/%3e%3cfeColorMatrix type='matrix' values='0 0 0 0 0.0392157 0 0 0 0 0.733333 0 0 0 0 0.937255 0 0 0 1 0'/%3e%3cfeBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_2109_5305'/%3e%3cfeBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow_2109_5305' result='shape'/%3e%3c/filter%3e%3c/defs%3e%3c/svg%3e ");
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  width: 123px;
+  height: 215px;
+  opacity: 0;
+  visibility: hidden;
+  transition: opacity 0.5s ease-in-out, visibility 0.5s ease-in-out;
+  &.show {
+    opacity: 1;
+    visibility: visible;
+  }
+
+  &[data-item="1"] {
+    right: 24%;
+    width: 6%;
+    height: 20%;
+    top: 3%;
+    z-index: 4;
+    transform: rotateZ(313deg);
+  }
+
+  &[data-item="2"] {
+    right: 31.6%;
+    width: 7%;
+    height: 20%;
+    top: 11%;
+    z-index: 4;
+    transform: rotateZ(270deg);
+  }
+
+  &[data-item="3"] {
+    right: 33%;
+    width: 7%;
+    height: 20%;
+    top: 19%;
+    z-index: 4;
+    transform: rotateZ(359deg);
+  }
+}
+
 .interactive-item {
   cursor: pointer;
-  transition: transform 0.2s ease-in-out;
+  transition: opacity 0.2s ease-in-out, filter 0.5s ease-in-out;
+  filter: drop-shadow(0px 6px 0px rgba(0, 0, 0, 0));
+  &.pulse {
+    filter: drop-shadow(0px 6px 10px #0078dc) drop-shadow(0px -6px 10px #0078dc);
+    animation: dropShadowPulse 3s linear infinite 0.4s;
+  }
 
   img {
     width: 100%;
