b571c8bede054ff537f89e0e1a3325e0058a2237
Ivan Tvisher <Tvisher@mail.ru>
2024-07-11 18:07:50 +0500

    add 3 module

--- src/components/ThirdStep.vue
+++ src/components/ThirdStep.vue
@@ -35,12 +35,12 @@
             <img src="@/assets/img/modules/module-3/object-3.svg" alt="" />
           </div>
 
-          <!-- <div
+          <div
             class="arrow-template"
             v-for="step in 3"
             :class="[moduleStep == step ? 'show' : '']"
             :data-item="step"
-          ></div> -->
+          ></div>
 
           <transition name="fade" mode="in-out">
             <div class="modal-template slider-modal" v-show="showSliderModal">
@@ -206,24 +206,11 @@
                           nextEl: '.album-next-btn',
                         }"
                       >
-                        <swiper-slide class="slide-item">
-                          <img
-                            src="https://sotni.ru/wp-content/uploads/2023/08/krasivye-pozitivnye-kartinki-2.webp"
-                            alt=""
-                          />
-                        </swiper-slide>
-                        <swiper-slide class="slide-item">
-                          <img
-                            src="https://lh3.googleusercontent.com/proxy/-2Vyb0CyidY6M85GGNm7HPVREaoMG4XPYwMggVn1LO99NhnAYD-Y9b3lkO_F6cC4DhFq6jlQf16Mhtz-NYIfPVWGxFVAtaK86_HRRT_NeF_j"
-                            alt=""
-                          />
-                        </swiper-slide>
-
-                        <swiper-slide class="slide-item">
-                          <img
-                            src="https://avatars.dzeninfra.ru/get-zen_doc/1657335/pub_5caa10891a50ee00afbe6392_5caa1091bd3a3000b0738124/scale_1200"
-                            alt=""
-                          />
+                        <swiper-slide
+                          class="slide-item"
+                          v-for="imagePath in albumImagesList"
+                        >
+                          <img :src="imagePath" alt="" />
                         </swiper-slide>
                         <template v-slot:container-start>
                           <div class="album-head">
@@ -375,6 +362,32 @@ const showPhotoAlbumModal = ref(false);
 
 const sliderPlayer = ref(null);
 
+const albumImagesList = [
+  "https://sotni.ru/wp-content/uploads/2023/08/krasivye-pozitivnye-kartinki-2.webp",
+  "https://lh3.googleusercontent.com/proxy/-2Vyb0CyidY6M85GGNm7HPVREaoMG4XPYwMggVn1LO99NhnAYD-Y9b3lkO_F6cC4DhFq6jlQf16Mhtz-NYIfPVWGxFVAtaK86_HRRT_NeF_j",
+  "https://avatars.dzeninfra.ru/get-zen_doc/1657335/pub_5caa10891a50ee00afbe6392_5caa1091bd3a3000b0738124/scale_1200",
+  "https://sotni.ru/wp-content/uploads/2023/08/krasivye-pozitivnye-kartinki-2.webp",
+  "https://lh3.googleusercontent.com/proxy/-2Vyb0CyidY6M85GGNm7HPVREaoMG4XPYwMggVn1LO99NhnAYD-Y9b3lkO_F6cC4DhFq6jlQf16Mhtz-NYIfPVWGxFVAtaK86_HRRT_NeF_j",
+  "https://avatars.dzeninfra.ru/get-zen_doc/1657335/pub_5caa10891a50ee00afbe6392_5caa1091bd3a3000b0738124/scale_1200",
+  "https://sotni.ru/wp-content/uploads/2023/08/krasivye-pozitivnye-kartinki-2.webp",
+  "https://lh3.googleusercontent.com/proxy/-2Vyb0CyidY6M85GGNm7HPVREaoMG4XPYwMggVn1LO99NhnAYD-Y9b3lkO_F6cC4DhFq6jlQf16Mhtz-NYIfPVWGxFVAtaK86_HRRT_NeF_j",
+  "https://avatars.dzeninfra.ru/get-zen_doc/1657335/pub_5caa10891a50ee00afbe6392_5caa1091bd3a3000b0738124/scale_1200",
+  "https://sotni.ru/wp-content/uploads/2023/08/krasivye-pozitivnye-kartinki-2.webp",
+  "https://lh3.googleusercontent.com/proxy/-2Vyb0CyidY6M85GGNm7HPVREaoMG4XPYwMggVn1LO99NhnAYD-Y9b3lkO_F6cC4DhFq6jlQf16Mhtz-NYIfPVWGxFVAtaK86_HRRT_NeF_j",
+  "https://avatars.dzeninfra.ru/get-zen_doc/1657335/pub_5caa10891a50ee00afbe6392_5caa1091bd3a3000b0738124/scale_1200",
+  "https://sotni.ru/wp-content/uploads/2023/08/krasivye-pozitivnye-kartinki-2.webp",
+  "https://lh3.googleusercontent.com/proxy/-2Vyb0CyidY6M85GGNm7HPVREaoMG4XPYwMggVn1LO99NhnAYD-Y9b3lkO_F6cC4DhFq6jlQf16Mhtz-NYIfPVWGxFVAtaK86_HRRT_NeF_j",
+  "https://avatars.dzeninfra.ru/get-zen_doc/1657335/pub_5caa10891a50ee00afbe6392_5caa1091bd3a3000b0738124/scale_1200",
+  "https://sotni.ru/wp-content/uploads/2023/08/krasivye-pozitivnye-kartinki-2.webp",
+  "https://lh3.googleusercontent.com/proxy/-2Vyb0CyidY6M85GGNm7HPVREaoMG4XPYwMggVn1LO99NhnAYD-Y9b3lkO_F6cC4DhFq6jlQf16Mhtz-NYIfPVWGxFVAtaK86_HRRT_NeF_j",
+  "https://avatars.dzeninfra.ru/get-zen_doc/1657335/pub_5caa10891a50ee00afbe6392_5caa1091bd3a3000b0738124/scale_1200",
+  "https://sotni.ru/wp-content/uploads/2023/08/krasivye-pozitivnye-kartinki-2.webp",
+  "https://lh3.googleusercontent.com/proxy/-2Vyb0CyidY6M85GGNm7HPVREaoMG4XPYwMggVn1LO99NhnAYD-Y9b3lkO_F6cC4DhFq6jlQf16Mhtz-NYIfPVWGxFVAtaK86_HRRT_NeF_j",
+  "https://avatars.dzeninfra.ru/get-zen_doc/1657335/pub_5caa10891a50ee00afbe6392_5caa1091bd3a3000b0738124/scale_1200",
+  "https://sotni.ru/wp-content/uploads/2023/08/krasivye-pozitivnye-kartinki-2.webp",
+  "https://lh3.googleusercontent.com/proxy/-2Vyb0CyidY6M85GGNm7HPVREaoMG4XPYwMggVn1LO99NhnAYD-Y9b3lkO_F6cC4DhFq6jlQf16Mhtz-NYIfPVWGxFVAtaK86_HRRT_NeF_j",
+  "https://avatars.dzeninfra.ru/get-zen_doc/1657335/pub_5caa10891a50ee00afbe6392_5caa1091bd3a3000b0738124/scale_1200",
+];
 const questionsList = [
   {
     id: 0,
@@ -522,6 +535,7 @@ onMounted(() => {
 }
 
 .arrow-template {
+  pointer-events: none;
   position: absolute;
   background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg width='123' height='215' viewBox='0 0 123 215' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cmask id='mask0_2109_5305' style='mask-type:alpha' maskUnits='userSpaceOnUse' x='0' y='0' width='123' height='215'%3e%3crect width='123' height='215' fill='%23D9D9D9'/%3e%3c/mask%3e%3cg mask='url(%23mask0_2109_5305)'%3e%3cg filter='url(%23filter0_d_2109_5305)'%3e%3cpath d='M87.9582 141.996L122.182 138.826L55.3048 213.776L16.3366 130.375L53.1999 139.875C53.1999 139.875 85.3189 64.9363 11.6033 1.59618C11.6033 1.59618 99.0172 34.3266 87.9582 141.996Z' fill='white'/%3e%3cpath d='M87.8383 142.117C98.0684 141.373 112.148 140.466 122.259 139.657L121.561 138.279C99.5735 162.253 75.7978 188.556 54.2251 212.814L56.6136 213.164C44.235 187.113 29.108 156.252 16.8057 130.168L16.2058 130.889C26.8613 133.666 42.2501 137.591 53.0473 140.477C56.69 135.535 57.3989 127.632 59.0239 121.477C68.5451 76.0544 47.7577 29.2216 12.7377 0.273624L10.9927 3.2362C65.8185 26.74 93.7931 83.9364 87.8383 142.117ZM88.0673 141.887C96.5742 87.8719 71.2062 30.3039 22.0843 4.69016C19.7286 3.55323 7.49174 -4.06639 10.4582 2.91917C50.0808 34.8516 70.7482 90.7033 52.6437 139.646L53.3417 139.296C42.37 137.055 26.3378 131.469 15.835 130.244C15.5951 130.649 25.4653 151.135 25.4762 151.507C33.9176 170.19 45.4347 195.76 53.9852 214.388C55.7847 218.772 70.181 197.957 73.0821 195.99C88.6017 178.487 107.491 157.072 122.804 139.384C125.967 135.721 90.8921 142.335 88.0782 141.887L88.0673 141.887Z' fill='black'/%3e%3c/g%3e%3c/g%3e%3cdefs%3e%3cfilter id='filter0_d_2109_5305' x='-0.00244141' y='0' width='123.008' height='215' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'%3e%3cfeFlood flood-opacity='0' result='BackgroundImageFix'/%3e%3cfeColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' result='hardAlpha'/%3e%3cfeOffset dx='-10'/%3e%3cfeComposite in2='hardAlpha' operator='out'/%3e%3cfeColorMatrix type='matrix' values='0 0 0 0 0.0392157 0 0 0 0 0.733333 0 0 0 0 0.937255 0 0 0 1 0'/%3e%3cfeBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_2109_5305'/%3e%3cfeBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow_2109_5305' result='shape'/%3e%3c/filter%3e%3c/defs%3e%3c/svg%3e ");
   background-repeat: no-repeat;
@@ -541,45 +555,27 @@ onMounted(() => {
     right: 54%;
     width: 6%;
     height: 20%;
-    top: 52%;
+    top: -3%;
     z-index: 4;
-    transform: rotateZ(190deg) scaleY(-1);
+    transform: rotateZ(-60deg);
   }
 
   &[data-item="2"] {
-    right: 37.6%;
+    right: 55.6%;
     width: 7%;
     height: 20%;
-    top: 27%;
+    top: 5%;
     z-index: 4;
     transform: rotateZ(341deg);
   }
 
   &[data-item="3"] {
-    right: 41%;
-    width: 7%;
-    height: 20%;
-    top: 2%;
+    left: 9%;
+    width: 6%;
+    height: 18%;
+    top: 67%;
     z-index: 4;
-    transform: rotateZ(317deg);
-  }
-
-  &[data-item="4"] {
-    right: 4%;
-    width: 7%;
-    height: 20%;
-    top: 12%;
-    z-index: 4;
-    transform: rotateZ(151deg) scaleX(-1);
-  }
-
-  &[data-item="5"] {
-    right: 62%;
-    width: 7%;
-    height: 20%;
-    top: 6%;
-    z-index: 4;
-    transform: rotateZ(-33deg);
+    transform: rotateZ(210deg) scaleY(-1);
   }
 }
 
