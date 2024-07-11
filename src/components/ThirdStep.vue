53523ee9830c539baa9c257042683025d3d84330
Ivan Tvisher <Tvisher@mail.ru>
2024-07-11 12:39:34 +0500

    add modal in 3 step

---
+++ src/components/ThirdStep.vue
@@ -0,0 +1,375 @@
+<template>
+  <transition name="slow-fade" mode="out-in">
+    <div
+      class="step-wrapper"
+      v-show="stepLoad"
+      :class="[isInteractive ? '' : 'no-interactive']"
+    >
+      <div class="step-wrapper__block">
+        <div class="step-wrapper__content">
+          <div class="content__part"></div>
+          <div
+            class="interactive-item"
+            data-item="1"
+            :class="[moduleStep == 1 ? 'pulse' : '']"
+            @click="openStep(2)"
+          >
+            <img src="@/assets/img/modules/module-3/object-1.svg" alt="" />
+          </div>
+
+          <div
+            class="interactive-item"
+            data-item="2"
+            :class="[moduleStep == 2 ? 'pulse' : '']"
+            @click="openStep(3)"
+          >
+            <img src="@/assets/img/modules/module-3/object-2.svg" alt="" />
+          </div>
+
+          <div
+            class="interactive-item"
+            data-item="3"
+            :class="[moduleStep == 3 ? 'pulse' : '']"
+            @click="openStep(4)"
+          >
+            <img src="@/assets/img/modules/module-3/object-3.svg" alt="" />
+          </div>
+
+          <!-- <div
+            class="arrow-template"
+            v-for="step in 3"
+            :class="[moduleStep == step ? 'show' : '']"
+            :data-item="step"
+          ></div> -->
+
+          <transition name="fade" mode="out-in">
+            <div class="modal-template slider-modal" v-if="showSliderModal">
+              <div class="modal-content">
+                <div class="man-modal__close" @click="compliteStep(1)"></div>
+                <div class="tabs-modal__inner">
+                  <div class="tabs-content">
+                    <swiper
+                      class="slider-el"
+                      :slides-per-view="1"
+                      :space-between="50"
+                      effect="fade"
+                      :fadeEffect="{
+                        crossFade: true,
+                      }"
+                      :navigation="{
+                        prevEl: '.slider-prev-btn',
+                        nextEl: '.slider-next-btn',
+                      }"
+                      :modules="[Navigation, Pagination, EffectFade]"
+                      :pagination="{ clickable: true }"
+                    >
+                      <swiper-slide class="slide-item">
+                        <span class="slide-item__title"
+                          >Узнайте о компании Хохланд</span
+                        >
+                        <div class="slide-item__inner">
+                          <div class="video-block">
+                            <vue-plyr @play="console.log('play')">
+                              <video
+                                crossorigin
+                                playsinline
+                                poster="@/assets/img/poster.png"
+                              >
+                                <source
+                                  src="@/assets/files/video-ex.mp4"
+                                  type="video/mp4"
+                                />
+                              </video>
+                            </vue-plyr>
+                          </div>
+                        </div>
+                      </swiper-slide>
+                      <swiper-slide class="slide-item">
+                        <span class="slide-item__title"
+                          >Заводы компании Хохланд в России</span
+                        >
+                        <div class="slide-item__inner">
+                          <div class="factory__wrapper">
+                            <div class="factory__image">
+                              <img
+                                src="../assets/img/modules/module-3/factory-1.jpg"
+                                alt=""
+                              />
+                            </div>
+                            <div class="factory__info">
+                              <div class="factory__title">
+                                Московская область
+                              </div>
+                              <div class="factory__descr">
+                                пос. РАОС, д. 16 140126 Раменский район
+                              </div>
+                              <div class="factory__text">
+                                Hochland производит плавленый сыр в России с
+                                2000 года. В ноябре 2003 года был введен в
+                                эксплуатацию новый завод в поселке РАОС,
+                                Московской области.
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </swiper-slide>
+                      <swiper-slide class="slide-item">
+                        <span class="slide-item__title"
+                          >Заводы компании Хохланд в России</span
+                        >
+                        <div class="slide-item__inner">
+                          <div class="factory__wrapper">
+                            <div class="factory__image">
+                              <img
+                                src="../assets/img/modules/module-3/factory-2.jpg"
+                                alt=""
+                              />
+                            </div>
+                            <div class="factory__info">
+                              <div class="factory__title">пос. Прохоровка</div>
+                              <div class="factory__descr">
+                                ул. Мичурина, д. 48 309000 Белгородская обл.
+                              </div>
+                              <div class="factory__text">
+                                В июле 2011 году Группа Hochland приобрела завод
+                                в п.Прохоровка, Белгородской области. С середины
+                                2012 года там производятся творожные сыры.
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </swiper-slide>
+                      <swiper-slide class="slide-item">
+                        <span class="slide-item__title"
+                          >Заводы компании Хохланд в России</span
+                        >
+                        <div class="slide-item__inner">
+                          <div class="factory__wrapper">
+                            <div class="factory__image">
+                              <img
+                                src="../assets/img/modules/module-3/factory-3.jpg"
+                                alt=""
+                              />
+                            </div>
+                            <div class="factory__info">
+                              <div class="factory__title">гор. Белинский</div>
+                              <div class="factory__descr">
+                                ул. Ленина, д. 28А 442250 Пензенская обл.
+                              </div>
+                              <div class="factory__text">
+                                Завод в Белинском, Пензенской области, вошел в
+                                состав Группы в ноябре 2017 года. После
+                                реконструкции на нем планируется производить
+                                продукцию для торговых сетей, а также сырье для
+                                завода в пос. РАОС.
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </swiper-slide>
+                      <div class="nav-buttons">
+                        <div class="slider-prev-btn">Назад</div>
+                        <div class="slider-next-btn">Дальше</div>
+                      </div>
+                    </swiper>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </transition>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script setup>
+import QuizModal from "@/components/QuizModal.vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Navigation, Pagination, EffectFade } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+import { ref, onMounted, computed } from "vue";
+
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+import { useGameStore } from "@/stores/GameStore.js";
+const store = useGameStore();
+
+const stepLoad = ref(false);
+const moduleStep = ref(null);
+const isInteractive = ref(false);
+
+const showSliderModal = ref(true);
+
+const openStep = (step) => {
+  const currentStep = moduleStep.value;
+  if (step == 1 && currentStep >= 1) {
+    showFileModal.value = true;
+  }
+};
+
+const compliteStep = (step) => {
+  if (step == 1) {
+    showFileModal.value = false;
+    if (moduleStep.value < 2) {
+      moduleStep.value = 2;
+    }
+  }
+};
+
+const compliteModule = () => {
+  store.openNewStep(4);
+  router.push("/");
+};
+
+onMounted(() => {
+  setTimeout(() => {
+    moduleStep.value = 1;
+    stepLoad.value = true;
+    isInteractive.value = true;
+  }, 500);
+});
+</script>
+
+<style lang="scss" scoped>
+.step-wrapper__content {
+  background-image: url(@/assets/img/modules/module-3/module-3-bg.svg);
+}
+.content__part {
+  z-index: 3;
+  pointer-events: none;
+  position: absolute;
+  background-image: url(@/assets/img/modules/module-3/module-3-bg-segment.svg);
+  background-repeat: no-repeat;
+  background-size: contain;
+  position: absolute;
+  height: 100%;
+  top: 0;
+  right: 0;
+  width: 73.6%;
+}
+
+.arrow-template {
+  position: absolute;
+  background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg width='123' height='215' viewBox='0 0 123 215' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cmask id='mask0_2109_5305' style='mask-type:alpha' maskUnits='userSpaceOnUse' x='0' y='0' width='123' height='215'%3e%3crect width='123' height='215' fill='%23D9D9D9'/%3e%3c/mask%3e%3cg mask='url(%23mask0_2109_5305)'%3e%3cg filter='url(%23filter0_d_2109_5305)'%3e%3cpath d='M87.9582 141.996L122.182 138.826L55.3048 213.776L16.3366 130.375L53.1999 139.875C53.1999 139.875 85.3189 64.9363 11.6033 1.59618C11.6033 1.59618 99.0172 34.3266 87.9582 141.996Z' fill='white'/%3e%3cpath d='M87.8383 142.117C98.0684 141.373 112.148 140.466 122.259 139.657L121.561 138.279C99.5735 162.253 75.7978 188.556 54.2251 212.814L56.6136 213.164C44.235 187.113 29.108 156.252 16.8057 130.168L16.2058 130.889C26.8613 133.666 42.2501 137.591 53.0473 140.477C56.69 135.535 57.3989 127.632 59.0239 121.477C68.5451 76.0544 47.7577 29.2216 12.7377 0.273624L10.9927 3.2362C65.8185 26.74 93.7931 83.9364 87.8383 142.117ZM88.0673 141.887C96.5742 87.8719 71.2062 30.3039 22.0843 4.69016C19.7286 3.55323 7.49174 -4.06639 10.4582 2.91917C50.0808 34.8516 70.7482 90.7033 52.6437 139.646L53.3417 139.296C42.37 137.055 26.3378 131.469 15.835 130.244C15.5951 130.649 25.4653 151.135 25.4762 151.507C33.9176 170.19 45.4347 195.76 53.9852 214.388C55.7847 218.772 70.181 197.957 73.0821 195.99C88.6017 178.487 107.491 157.072 122.804 139.384C125.967 135.721 90.8921 142.335 88.0782 141.887L88.0673 141.887Z' fill='black'/%3e%3c/g%3e%3c/g%3e%3cdefs%3e%3cfilter id='filter0_d_2109_5305' x='-0.00244141' y='0' width='123.008' height='215' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'%3e%3cfeFlood flood-opacity='0' result='BackgroundImageFix'/%3e%3cfeColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' result='hardAlpha'/%3e%3cfeOffset dx='-10'/%3e%3cfeComposite in2='hardAlpha' operator='out'/%3e%3cfeColorMatrix type='matrix' values='0 0 0 0 0.0392157 0 0 0 0 0.733333 0 0 0 0 0.937255 0 0 0 1 0'/%3e%3cfeBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_2109_5305'/%3e%3cfeBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow_2109_5305' result='shape'/%3e%3c/filter%3e%3c/defs%3e%3c/svg%3e ");
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  width: 123px;
+  height: 215px;
+  opacity: 0;
+  visibility: hidden;
+  transition: opacity 0.5s ease-in-out, visibility 0.5s ease-in-out;
+  &.show {
+    opacity: 1;
+    visibility: visible;
+  }
+
+  &[data-item="1"] {
+    right: 54%;
+    width: 6%;
+    height: 20%;
+    top: 52%;
+    z-index: 4;
+    transform: rotateZ(190deg) scaleY(-1);
+  }
+
+  &[data-item="2"] {
+    right: 37.6%;
+    width: 7%;
+    height: 20%;
+    top: 27%;
+    z-index: 4;
+    transform: rotateZ(341deg);
+  }
+
+  &[data-item="3"] {
+    right: 41%;
+    width: 7%;
+    height: 20%;
+    top: 2%;
+    z-index: 4;
+    transform: rotateZ(317deg);
+  }
+
+  &[data-item="4"] {
+    right: 4%;
+    width: 7%;
+    height: 20%;
+    top: 12%;
+    z-index: 4;
+    transform: rotateZ(151deg) scaleX(-1);
+  }
+
+  &[data-item="5"] {
+    right: 62%;
+    width: 7%;
+    height: 20%;
+    top: 6%;
+    z-index: 4;
+    transform: rotateZ(-33deg);
+  }
+}
+
+.interactive-item {
+  cursor: pointer;
+  transition: opacity 0.2s ease-in-out, filter 0.5s ease-in-out;
+  filter: drop-shadow(0px 6px 0px rgba(0, 0, 0, 0));
+  position: absolute;
+  &.pulse {
+    filter: drop-shadow(0px 6px 10px #0078dc) drop-shadow(0px -6px 10px #0078dc);
+    animation: dropShadow 3s linear infinite 0.4s;
+  }
+
+  img {
+    width: 100%;
+    object-fit: contain;
+    height: 100%;
+  }
+
+  &[data-item="1"] {
+    right: 20.4%;
+    width: 34.4%;
+    height: 35%;
+    top: 1.7%;
+    &.pulse {
+      animation: none;
+    }
+  }
+
+  &[data-item="2"] {
+    right: 44.7%;
+    width: 21%;
+    height: 30%;
+    top: 22.7%;
+  }
+
+  &[data-item="3"] {
+    left: 0%;
+    bottom: 0%;
+    width: 17%;
+    height: 22%;
+  }
+
+  &[data-item="4"] {
+    right: 50.5%;
+    width: 13%;
+    height: 24%;
+    top: 72%;
+  }
+
+  &[data-item="5"] {
+    right: 55%;
+    width: 13%;
+    height: 22%;
+    top: 27%;
+  }
+}
+.video-block {
+  margin-left: 0;
+}
+</style>
--- src/pages/StepPage.vue
+++ src/pages/StepPage.vue
@@ -2,6 +2,7 @@
   <div>
     <FirstStep v-if="step == 1" />
     <SecondStep v-if="step == 2" />
+    <ThirdStep v-if="step == 3" />
   </div>
 </template>
 
@@ -9,6 +10,8 @@
 import { ref } from "vue";
 import FirstStep from "@/components/FirstStep.vue";
 import SecondStep from "@/components/SecondStep.vue";
+import ThirdStep from "@/components/ThirdStep.vue";
+
 import { useRoute } from "vue-router";
 const route = useRoute();
 const step = ref(route.params.id);
