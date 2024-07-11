3a283cf1598d284d9d5091d4028594baecd47546
Ivan Tvisher <Tvisher@mail.ru>
2024-07-11 09:27:49 +0500

    add second step

--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -148,7 +148,7 @@ onMounted(() => {
     position: absolute;
     left: 50%;
     top: 50%;
-    transform: translate(-50%, -50%);
+    transform: translate3d(-50%, -50%, 0px);
     width: 100%;
     height: 100%;
     filter: grayscale(100);
